596b15c5fbfb70557885cec81b09bf34690beec4
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-08-26 22:44:22 +0900

    keyの追加

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,21 @@ type TodoType = {
 };
 
 function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) => {
       console.log(res);
       setTodos(res.data);
     });
   };
+
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
-      {todos.map((todo) => {
+      {todos.map((todo, index) => {
         return (
-          <div>
-            <Todo title={todo.title} userid={todo.userid} />
+          <div key={index}>
+            <Todo title={todo.title} userid={todo.userId} />
           </div>
         );
       })}
